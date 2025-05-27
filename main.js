6392c173a97bd84b37aaee9f36b4e414e8e22af1
PabloElidio <145361911+PabloElidio@users.noreply.github.com>
2025-05-27 11:41:49 -0300

    .

--- main.js
+++ main.js
@@ -16,3 +16,6 @@ for (let i = 0; i <botoes.length; i++){
 
 const contadores = document.querySelectorAll("contador");
 const tempoObjetivo1 = new Date ("2025-10-05t00:00:00");
+let tempoAtual = new Date ();
+
+contadores[0].textContent = calculaTempo (tempoObjetivo1);
